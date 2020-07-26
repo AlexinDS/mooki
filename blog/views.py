aa2a6f39432261023004d7005d3ef1b166347451
Admin <alexin.fr@gmail.com>
2020-07-26 15:36:16 +0200

    1.1

--- blog/views.py
+++ blog/views.py
@@ -77,7 +77,6 @@ def Nouvel_Article(request):
             article = form.save(commit=False)
             article.auteur = request.user
             article.date = timezone.now()
-            article.photo = []
             article.save()
         return render(request, 'accueil.html', {'form': form})
     else:
